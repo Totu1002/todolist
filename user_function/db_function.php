1bc31999896643616076e7fa18ee46361cc1de5f
Totu1002_Mac <totu.enjoy.music@gmail.com>
2021-09-20 23:49:54 +0900

    [add]セッション機能確認完了

--- user_function/db_function.php
+++ user_function/db_function.php
@@ -6,7 +6,7 @@ class DbFunction{
 			$pdo = new PDO('sqlite:Login.sqlite3');
 			$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 			$pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
-			//echo('ok');
+			echo('--- db conection ok ---' . "\n");
 			return $pdo;
 		}catch (Exception $e){
 			echo ($e->getMessage());
@@ -17,19 +17,47 @@ class DbFunction{
 		$dbh = $this->db_conection();
 		$sql = "CREATE TABLE IF NOT EXISTS users(id INTEGER PRIMARY KEY AUTOINCREMENT,name VARCHAR(10),pass TEXT, mail TEXT)";
 		$stmt = $dbh->prepare($sql);
-    	$stmt->execute();
+    $stmt->execute();
 	}
 
 	public function db_create_tasks_table(){
 		$dbh = $this->db_conection();
-		$sql = "CREATE TABLE IF NOT EXISTS tasks(id INTEGER PRIMARY KEY AUTOINCREMENT,title VARCHAR(10), title VARCHAR(30), done INT)";
+		$sql = "CREATE TABLE IF NOT EXISTS tasks(id INTEGER PRIMARY KEY AUTOINCREMENT, user_id INTEGER, title VARCHAR(10), body VARCHAR(30), done INTEGER)";
 		$stmt = $dbh->prepare($sql);
-    	$stmt->execute();
+    $stmt->execute();
 	}
 	
-	public function db_select(){
+	//TODO パスワード保存する際はハッシュ保存とするべき
+	public function db_insert_default_user(){
+		$dbh = $this->db_conection();
+		$sql = "INSERT INTO users(name, pass, mail) VALUES ('dev_user', 'dev_user', 'dev_user@test.com')";
+		$stmt = $dbh->prepare($sql);
+    $stmt->execute();
+	}
+
+	public function db_select($sql){
+		$dbh = $this->db_conection();
+		//$sql = "SELECT * FROM users";
+		//var_dump($sql);
+		$stmt = $dbh->query($sql);
+    	$items = $stmt->fetchAll();
+    	return $items;
+	}
+
+	//不要
+	public function db_select_users(){
 		$dbh = $this->db_conection();
 		$sql = "SELECT * FROM users";
+		//var_dump($sql);
+		$stmt = $dbh->query($sql);
+    	$items = $stmt->fetchAll();
+    	return $items;
+	}
+
+	//不要
+	public function db_select_tasks(){
+		$dbh = $this->db_conection();
+		$sql = "SELECT * FROM tasks";
 		//var_dump($sql);
 		$stmt = $dbh->query($sql);
     	$items = $stmt->fetchAll();
--- user_function/db_operation.php
+++ user_function/db_operation.php
@@ -7,8 +7,26 @@ class DbOperation{
 }
 
 $dbh = new DbFunction;
-$dbh->db_create_users_table();
-$dbh->db_create_tasks_table();
-$select_items = $dbh->db_select();
-var_dump($select_items);
+//$dbh->db_conection();
+
+//新規テーブル作成用処理
+//$dbh->db_create_users_table();
+//$dbh->db_create_tasks_table();
+
+//$select_users = $dbh->db_select_users();
+//var_dump($select_users);
+//$select_tasks = $dbh->db_select_tasks();
+//var_dump($select_tasks);
+
+//TODO
+//テスト用デフォルトユーザー作成処理
+//$dbh->db_insert_default_user();
+
+$select_users = 'SELECT * FROM users';
+$users_items = $dbh->db_select($select_users);
+var_dump($users_items);
+
+$select_tasks = 'SELECT * FROM tasks';
+$tasks_items = $dbh->db_select($select_tasks);
+var_dump($tasks_items);
 ?>
